1c7168f270fcdba365b1554582c0e10728dd4f29
idbmb <bmb.router@gmail.com>
2019-11-19 06:38:33 +0700

    rebuilding site Sel Nov 19 06:38:33 WIB 2019

---
+++ pu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -e "\033[0;32mPushing updates to GitHub...\033[0m"
+
+# tambahkan perubahan ke Git
+git add .
+
+# Buat sebuah commit baru
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push atau upload ke Github
+git push -u origin master
